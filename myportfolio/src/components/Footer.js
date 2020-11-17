f8141384d8b69bf0511f5cd60566b40f957ff973
SBuchanan1 <syd.buch@gmail.com>
2020-11-17 02:49:49 -0500

    added footer

---
+++ myportfolio/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./components.css";
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="page-footer">
+                    <h6 id="footText">By Sydney Buchanan</h6>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Footer;
